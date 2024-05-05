e7339d956f8e8b0508f0c8011dec3748ff438d8d
thetheOrange <13807527538@163.com>
2024-05-05 11:15:57 +0800

    test: 增加对log日志和config文件修改的测试

---
+++ Tests/config_test.py
@@ -0,0 +1,17 @@
+from Core.config import Config
+
+config = Config()
+#
+# print(type(config.read()), config.read())
+#
+# print(config.modify(key="a", value=2))
+#
+# print(type(config.read()), config.read())
+#
+# print(config.modify(key="b", value=3))
+
+print(f"{config:init}")
+print(f"{config:path}")
+print(f"{config:config}")
+print(config)
+
---
+++ Tests/log_test.py
@@ -0,0 +1,4 @@
+from Logging import app_logger
+
+if __name__ == "__main__":
+    app_logger.info("test")
