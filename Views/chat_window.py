cec1e50b8faea8cfb6220a8d9dfd74c2c9658d3e
joana123123 <140952695+joana123123@users.noreply.github.com>
2024-06-03 19:08:36 +0800

    test:chatwindow修改

--- Views/chat_window.py
+++ Views/chat_window.py
@@ -13,7 +13,9 @@ from qfluentwidgets import SplitFluentWindow, FluentIcon
 from chat import ChatWidget
 from qfluentwidgets import (NavigationInterface, NavigationItemPosition, NavigationWidget, MessageBox,
                             isDarkTheme, setTheme, Theme, setThemeColor, qrouter, FluentWindow, NavigationAvatarWidget)
-
+from MaskWidget import MaskWidget
+from SettingWindow import SettingWidget
+from qfluentwidgets import FluentIcon as FIF
 class MainChatWindow(SplitFluentWindow):
     """
     聊天主界面
@@ -21,30 +23,20 @@ class MainChatWindow(SplitFluentWindow):
 
     def __init__(self):
         super().__init__()
-
+        self.setObjectName("MainChatWindow")
         self.setWindowTitle("聊天主界面")
-
-
-
-
         #导航栏按钮
         self.photohpath='../Asserts/icons/user.png'
-        self.navigationInterface.addWidget(
-            routeKey='settings',
-            widget=NavigationAvatarWidget('个人中心', self.photohpath),
-            onClick=self.settingwidget, # 槽函数
-            position=NavigationItemPosition.BOTTOM,
-        )
-        self.photohpath2='../Asserts/icons/user.png'
-        self.navigationInterface.addWidget(
-            routeKey='mask',
-            widget=NavigationAvatarWidget('面具',  self.photohpath2),
-            onClick=self.maskwidget,
-            position=NavigationItemPosition.TOP,
-        )
+        # self.navigationInterface.addWidget(
+        #     routeKey='settings',
+        #     widget=NavigationAvatarWidget('个人中心', self.photohpath),
+        #     onClick=self.settingwidget, # 槽函数
+        #     position=NavigationItemPosition.BOTTOM,
+        # )
+        self.photohpath2=r'../Asserts/icons/user.png'
         self.navigationInterface.addWidget(
             routeKey='insertion',
-            widget=NavigationAvatarWidget('插件', self.photohpath2),
+            widget=NavigationAvatarWidget('插件',  self.photohpath2),
             onClick=self.showMessageBox,
             position=NavigationItemPosition.TOP,
         )
@@ -54,12 +46,17 @@ class MainChatWindow(SplitFluentWindow):
             onClick=self.newchat,
             position=NavigationItemPosition.BOTTOM,
         )
+        #面具页面
+        self.maskwidget = MaskWidget()
+        self.addSubInterface( self.maskwidget ,FluentIcon.ROBOT , '面具', NavigationItemPosition.TOP)
+        self.SettingWidget= SettingWidget()
+        self.addSubInterface(self.SettingWidget, FIF.SETTING, '设置', NavigationItemPosition.BOTTOM)
 
     def settingwidget(self):
         print('hi')
 
-    def maskwidget(self):
-        print('hi')
+
+
     def newchat(self):
         # 添加子界面
         self.sub_chat_window = ChatWidget()
