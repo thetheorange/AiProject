5c4143719a9755c5209b5f63c605ff2b8df800e1
joana123123 <140952695+joana123123@users.noreply.github.com>
2024-06-03 13:51:42 +0800

    test: chat_window不能正常

---
+++ Views/chat_window.py
@@ -0,0 +1,82 @@
+"""
+Des 聊天主界面
+@Author thetheOrange
+Time 2024/5/25
+"""
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,QPushButton
+from qfluentwidgets import SplitFluentWindow, FluentIcon
+
+from sub_chat_window import SubChatWindow
+from chat import Test,ChatWidget
+from qfluentwidgets import (NavigationInterface, NavigationItemPosition, NavigationWidget, MessageBox,
+                            isDarkTheme, setTheme, Theme, setThemeColor, qrouter, FluentWindow, NavigationAvatarWidget)
+
+class MainChatWindow(SplitFluentWindow):
+    """
+    聊天主界面
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("聊天主界面")
+
+        # 添加子界面
+        self.sub_chat_window = ChatWidget()
+        # 第一个参数 添加的子界面对象， 第二个参数 导航栏所呈现的icon(建议直接用fluent框架提供的icon) 第三个参数 导航栏的所呈现的标题
+        self.addSubInterface(self.sub_chat_window, FluentIcon.ADD_TO, "聊天")
+
+
+        #导航栏按钮
+        self.photohpath='../Asserts/icons/user.png'
+        self.navigationInterface.addWidget(
+            routeKey='avatar',
+            widget=NavigationAvatarWidget('个人中心', self.photohpath),
+            onClick=self.showMessageBox, # 槽函数
+            position=NavigationItemPosition.BOTTOM,
+        )
+        self.photohpath2='../Asserts/icons/user.png'
+        self.navigationInterface.addWidget(
+            routeKey='avatar2',
+            widget=NavigationAvatarWidget('面具',  self.photohpath2),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.TOP,
+        )
+        self.navigationInterface.addWidget(
+            routeKey='avatar2',
+            widget=NavigationAvatarWidget('面具', self.photohpath2),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.SCROLL,
+        )
+        self.navigationInterface.addWidget(
+            routeKey='avatar4',
+            widget=NavigationAvatarWidget('插件', self.photohpath2),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.SCROLL,
+        )
+        self.navigationInterface.addWidget(
+            routeKey='avatar3',
+            widget=NavigationAvatarWidget('新的聊天', self.photohpath2),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.BOTTOM,
+        )
+
+
+    def showMessageBox(self):
+        #切换设置页面
+        print('hi')
+
+
+
+if __name__ == "__main__":
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+    w = MainChatWindow()
+    w.show()
+    sys.exit(app.exec_())
