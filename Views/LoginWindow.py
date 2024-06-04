88fe1349d912d26ef13743b9a268bbc9782b97e4
thetheOrange <13807527538@163.com>
2024-06-04 22:26:23 +0800

    refactor: 重构注册和登录界面

---
+++ Views/LoginWindow.py
@@ -0,0 +1,220 @@
+"""
+Des 登录窗口
+@Author thetheOrange
+Time 2024/6/4
+"""
+import json
+import sys
+
+import requests
+from PyQt5.QtCore import Qt, QPoint, QSize, QEventLoop, QTimer
+from PyQt5.QtGui import QPixmap, QIcon, QMouseEvent, QMovie
+from PyQt5.QtWidgets import QApplication, QWidget, QAction
+from PyQt5.uic import loadUi
+from qfluentwidgets import FluentIcon, CommandBar, PushButton, LineEdit, PasswordLineEdit, InfoBar, InfoBarPosition, \
+    SplashScreen
+from qfluentwidgets.common.icon import Icon
+
+from Views.RegisterWindow import RegisterWindow
+
+
+class LoginWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        loadUi("../Templates/login.ui", self)
+
+        # =============================================基础设置start=============================================
+
+        self.setWindowTitle("登录")
+        self.setWindowIcon(QIcon("../Assets/Icons/sign.png"))
+        self.setCursor(Qt.SizeAllCursor)
+
+        # 设置窗口无边框
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # 设置窗口透明化
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        # 设置窗口鼠标追踪
+        self.setMouseTracking(True)
+
+        # 鼠标偏移量
+        self.__change_pos = None
+        # 鼠标起始值
+        self.__start_pos = None
+        # 鼠标移动标志
+        self.__move_flag = False
+
+        # 设置登录和注册按钮手势
+        self.login_button: PushButton
+        self.register_link: PushButton
+        self.login_button.setCursor(Qt.PointingHandCursor)
+        self.register_link.setCursor(Qt.PointingHandCursor)
+
+        # 注册窗口
+        self.register_win: RegisterWindow = RegisterWindow()
+
+        # =============================================基础设置end=============================================
+
+        # =============================================图片设置start=============================================
+
+        window_icon: str = "../Assets/Image/logo_orange.png"
+        background_img: str = "../Assets/image/background.jpg"
+        logo_img: str = "../Assets/image/logo.svg"
+        self.window_icon.setImage(QPixmap(window_icon).scaled(25, 25, Qt.KeepAspectRatio,
+                                                              Qt.SmoothTransformation))
+        self.banner.setImage(QPixmap(background_img).scaled(400, 400, Qt.KeepAspectRatio,
+                                                            Qt.SmoothTransformation))
+        self.logo.setImage(logo_img)
+
+        # =============================================图片设置end=============================================
+
+        # =============================================启动界面start=============================================
+
+        # 1. 创建启动页面
+        self.splashScreen = SplashScreen(QIcon(window_icon), self)
+        self.splashScreen.setIconSize(QSize(200, 200))
+        # 2. 在创建其他子页面前先显示主界面
+        self.show()
+        # 3. 创建子界面
+        self.create_sub_interface()
+        # 4. 隐藏启动页面
+        self.splashScreen.finish()
+
+        # =============================================启动界面end=============================================
+
+        # =============================================CommandBar设置start=============================================
+
+        self.command_bar: CommandBar
+        # 最小化按钮
+        min_btn: QAction = QAction(triggered=lambda x: self.showMinimized())
+        min_btn.setIcon(Icon(FluentIcon.MINIMIZE))
+        # 退出按钮
+        exit_btn: QAction = QAction(triggered=lambda x: self.close())
+        exit_btn.setIcon(Icon(FluentIcon.CLOSE))
+        self.command_bar.setCursor(Qt.PointingHandCursor)
+        self.command_bar.addAction(min_btn)
+        self.command_bar.addAction(exit_btn)
+
+        # =============================================CommandBar设置end=============================================
+
+        self.__bind_signal()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if self.__move_flag:
+            # 计算鼠标偏移量
+            self.__change_pos = event.pos() - self.__start_pos
+            # 移动窗口
+            self.move(self.pos() + self.__change_pos)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if Qt.LeftButton:
+            # 获取鼠标当前位置
+            self.__start_pos = QPoint(event.x(), event.y())
+            # 设置鼠标移动状态为True
+            self.__move_flag = True
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton:
+            # 释放鼠标位置信息并将移动状态改为False
+            self.__move_flag = False
+            self.__start_pos = None
+            self.__change_pos = None
+
+    def __bind_signal(self) -> None:
+        """
+        绑定信号与槽
+
+        :return:
+        """
+        self.login_button.clicked.connect(self.login)
+        self.register_link.clicked.connect(lambda x: self.register_win.show())
+
+    def login(self) -> None:
+        """
+        登录事件
+
+        :return:
+        """
+        self.user_name_input: LineEdit  # 用户名
+        self.pwd_input: PasswordLineEdit  # 用户密码
+        # 登录接口
+        login_api: str = r"http://127.0.0.1:5000/auth/login"
+
+        user_name: str = self.user_name_input.text()
+        user_pwd: str = self.pwd_input.text()
+
+        if not user_name and not user_pwd:
+            InfoBar.error(
+                title="登录状态",
+                content="输入不能为空",
+                orient=Qt.Vertical,
+                isClosable=True,
+                position=InfoBarPosition.BOTTOM_RIGHT,
+                duration=1000,
+                parent=self
+            )
+        else:
+            r = requests.post(url=login_api,
+                              headers={
+                                  "Content-Type": "application/json"
+                              },
+                              data=json.dumps({
+                                  "username": user_name,
+                                  "password": user_pwd
+                              }))
+            print(r.content.decode())
+            if r.status_code == 200:
+                # 检查是否登录成功
+                code: int | str = r.json()["code"]
+                if code != 0:
+                    InfoBar.error(
+                        title="登录状态",
+                        content="登录错误",
+                        orient=Qt.Vertical,
+                        isClosable=True,
+                        position=InfoBarPosition.BOTTOM_RIGHT,
+                        duration=1000,
+                        parent=self
+                    )
+                else:
+                    InfoBar.success(
+                        title="登录状态",
+                        content="登录成功",
+                        orient=Qt.Vertical,
+                        isClosable=True,
+                        position=InfoBarPosition.BOTTOM_RIGHT,
+                        duration=1000,
+                        parent=self
+                    )
+            else:
+                InfoBar.error(
+                    title="登录状态",
+                    content=f"网络异常 {r.status_code}",
+                    orient=Qt.Vertical,
+                    isClosable=True,
+                    position=InfoBarPosition.BOTTOM_RIGHT,
+                    duration=1000,
+                    parent=self
+                )
+
+    def create_sub_interface(self) -> None:
+        """
+        启动界面
+
+        :return:
+        """
+
+        loop = QEventLoop(self)
+        QTimer.singleShot(1000, loop.quit)
+        loop.exec()
+
+
+if __name__ == "__main__":
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+    w = LoginWindow()
+    w.show()
+    sys.exit(app.exec_())
