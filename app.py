915d2317a7c0d238e5e1c1678542c90a467bb30e
thetheOrange <13807527538@163.com>
2024-06-08 21:05:07 +0800

    pref: 加入上传的文件限制

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from Api.auth import auth_blu
 from Api.model import model_blu
 
 app = Flask(__name__)
+# 限制上传文件的最大大小为16k
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 app.register_blueprint(auth_blu)
 app.register_blueprint(model_blu)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5120)
