af735b2ef6741e4a170611700f88b09c4af78295
lingsui123 <1289205640@qq.com>
2024-11-29 22:29:18 +0800

    aaaaa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
@@ -7,6 +8,13 @@ from Views.MainWindow import MainWindow
 
 # 程序入口
 if __name__=="__main__":
+    if not os.path.exists('./Temp'):
+        # 目录不存在，创建目录
+        os.makedirs('./Temp')
+        print(f"目录 '{'./Temp'}' 已创建。")
+    else:
+        # 目录已存在
+        print(f"目录 '{'./Temp'}' 已存在。")
     QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
